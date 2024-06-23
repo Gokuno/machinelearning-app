ef263de231276c4155ce9424c7587ec313d26dcf
Allan Paz <jallanpazg@gmail.com>
2024-06-22 23:31:29 -0700

    Update Translation.jsx

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -11,7 +11,7 @@ export default function Translation(props) {
             {!translating && (<div className='flex flex-col gap-1 mb-4'>
                 <p className='text-xs sm:text-sm font-medium text-slate-500 mr-auto'>To language</p>
                 <div className='flex items-stretch gap-2 sm:gap-4' >
-                    <select value={toLanguage} className='flex-1 outline-none w-full focus:outline-none bg-white duration-200 p-2  rounded' onChange={(e) => setToLanguage(e.target.value)}>
+                    <select value={toLanguage} className='flex-1 outline-none w-full focus:outline-none bg-white duration-200 p-2 rounded sm:text-sm' onChange={(e) => setToLanguage(e.target.value)}>
                         <option value={'Select language'}>Select language</option>
                         {Object.entries(LANGUAGES).map(([key, value]) => {
                             return (
